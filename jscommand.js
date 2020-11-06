774886511e14cf3b17c90329a47d259d8a5161f8
Ben-jpeg <72921912+Ben-jpeg@users.noreply.github.com>
2020-11-06 17:17:17 +0100

    Add files via upload

---
+++ jscommand.js
@@ -0,0 +1,54 @@
+//Déclarer deux variables avec comme valeur 4 et 5
+//Créez une autre variable contenant l'addition des deux et l'afficher.
+// const age1 = 5;
+// let age2 = 4;
+
+// const Addit = age1 + age2;
+// console.log(Addit); 
+
+
+//-------------------------
+//Afficher la chaine de caractère suivante en majuscule
+// const team = 'avengers';
+// const teamMaj = 'avengers'.toUpperCase()
+
+
+// console.log(teamMaj)
+
+//----------------------------------
+//afficher la première lettre de la chaine de caractère suivante
+//const team = 'avengers';
+//const team1 = 'avengers' [0];
+
+//console.log(team1)
+
+//--------------------------------------
+
+//afficher la chaine de caractère suivante exepté la première lettre.
+//const team = 'avengers';
+//const team1 = 'avengers'
+
+//console.log(team1[1], team1[2], team1[3], team1[4], team1[5], team1[6], team1[7]);
+
+
+//-----------------------------------------
+//Afficher les deux chaines suivantes dans une seule chaine de caractère (concaténation)
+//const firstName = 'Tony';
+//const lastName = 'Stark';
+
+//console.log(firstName + lastName);
+
+
+//------------------------------------------
+
+//Afficher la chaine de caractère suivante avec seulement la première lettre en majuscule.
+//=> Avengers
+//const team = 'aveNgers'.toUpperCase()[0];
+
+
+//const team1 = 'vengers'
+
+//console.log(team + team1);
+
+
+//---------------------------------
