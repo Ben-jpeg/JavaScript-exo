b2473826d4ed9db7e17346117aa64f1eb5a5f307
Ben-jpeg <72921912+Ben-jpeg@users.noreply.github.com>
2020-11-08 14:10:19 +0100

    Add files via upload

--- jscommand.js
+++ jscommand.js
@@ -1,10 +1,10 @@
 //Déclarer deux variables avec comme valeur 4 et 5
 //Créez une autre variable contenant l'addition des deux et l'afficher.
-// const age1 = 5;
-// let age2 = 4;
+//const age1 = 5;
+//let age2 = 4;
 
-// const Addit = age1 + age2;
-// console.log(Addit); 
+//const Addit = age1 + age2;
+//console.log(Addit);
 
 
 //-------------------------
@@ -52,3 +52,89 @@
 
 
 //---------------------------------
+//2e try exercices Dim08nov
+//---------------------------------
+
+//Créez une autre variable contenant l'addition des deux et l'afficher.
+//const age1 = 5;
+//let age2 = 4;
+
+//let nb1 = 4;
+//let nb2 = 5;
+
+//console.log(nb1 + nb2);
+
+//--------------------------
+
+//Afficher la chaine de caractère suivante en majuscule
+//let team = 'avengers';
+
+//let team = 'avengers';
+
+//console.log(team.toUpperCase());
+
+//-------------------------
+
+//Afficher la première lettre de la chaine de caractère suivante
+//let team = 'a';
+//let teamA = 'vengers'
+
+//console.log(team[0];
+
+//-------------------------
+
+//afficher la chaine de caractère suivante excepté la première lettre.
+//let team = 'avengers';
+//let team1 = 'vengers'
+
+//console.log(team1);
+
+//--------------------------------
+
+//Afficher les deux chaines suivantes dans une seule chaine de caractère (concaténation)
+//let firstName = 'Tony';
+//let lastName = 'Stark';
+
+//console.log(firstName + lastName);
+
+//----------------------------------
+
+
+//Afficher la chaine de caractère suivante avec seulement la première lettre en majuscule.
+//let team = 'aveNgers';
+//=> Avengers
+
+//console.log(team.charAt(0).toUpperCase() + team.substring(1).toLowerCase());
+
+//---------------------------------
+//PAUSE CAFEW ---------------------------------------
+//---------------------------------
+
+//Les tableaux
+//Déclarez un tableau avec 5 villes
+//Afficher la ville à l'indice 3
+//Ajouter une ville au tableau
+//Supprimer la ville à l'indice 2
+
+//var villes = ["Marseille", "Bordeaux", "Montpellier", "Nice", "Paris"]
+//console.log(villes[3]);
+
+//var villes = ["Marseille", "Bordeaux", "Montpellier", "Nice", "Paris", "Auxerre"]
+//console.log(villes[5]);
+
+//var villes = ["Marseille", "Bordeaux", "Nice", "Paris", "Auxerre"]
+
+//----------------------------------
+
+//Les boucles
+//
+//Comme tu le sais sans doute, pour parcourir un tableau,
+//il faut boucler sur ce tableau. Parcours le tableau suivant afin de monter chaque
+//élément au carré et affiche le tableau.
+
+const array1 = [2, 4, 8];
+
+array1.forEach(element => console.log(element));
+
+function carré(2, 4, 8);
+return 2 * 4 * 8;
